cfcaa0f5ade4beac6a543046624a930e9eaa9229
LauraBallesteros95 <l.ballesteros.zafra@gmail.com>
2023-12-04 00:32:37 +0100

    :modif the file to push in github

--- SQL Lab2.sql
+++ SQL Lab2.sql
@@ -22,3 +22,47 @@ select count(store_id) as store_count from store;
 select count(staff_id) as staff_count from staff;
 -- Return a list of employee first names only?
 select first_name from staff;
+
+-- LAB 2
+
+-- Select all the actors with the first name ‘Scarlett’.
+-- Select all the actors with the last name ‘Johansson’.
+-- How many films (movies) are available for rent?
+-- How many films have been rented?
+-- What is the shortest and longest rental period?
+-- What are the shortest and longest movie duration? Name the values max_duration and min_duration.
+-- What's the average movie duration?
+-- What's the average movie duration expressed in format (hours, minutes)?
+-- How many movies longer than 3 hours?
+-- Get the name and email formatted. Example: Mary SMITH - mary.smith@sakilacustomer.org.
+-- What's the length of the longest film title?
+
+-- Select all the actors with the first name ‘Scarlett’.
+select * from actor where first_name = 'Scarlett';
+-- Select all the actors with the last name ‘Johansson’.
+select * from actor where last_name = 'Johansson';
+-- How many films (movies) are available for rent?
+select count(*) as film_count from film where rental_duration > 0;
+-- How many films have been rented?
+select count(distinct inventory_id) as film_count from rental;
+-- What is the shortest and longest rental period?
+-- 
+-- What are the shortest and longest movie duration? Name the values max_duration and min_duration.
+select min(length) as min_duration, max(length) as max_duration from film;
+-- What's the average movie duration?
+select avg(length) as average_duration from film;
+-- What's the average movie duration expressed in format (hours, minutes)?
+select concat(
+       floor(avg(length)/60), 'hours', 
+       mod(avg(length), 60), 'minutes'
+       ) as average_duration 
+from film;
+-- How many movies longer than 3 hours?
+select count(*) as movies_count
+from film
+where length > 180;
+-- Get the name and email formatted. Example: Mary SMITH - mary.smith@sakilacustomer.org.
+select concat(first_name, ' ' , last_name, '-', email) as formatted_info
+from customer;
+-- What's the length of the longest film title?
+select max(length(title)) as longest_title_length from film;
